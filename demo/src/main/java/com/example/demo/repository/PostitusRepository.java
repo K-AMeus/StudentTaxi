e53dfe65e137b2096ce2e607f9563d37248c669f
Meus <karl.andreas@meus.ee>
2024-03-22 17:59:49 +0200

    Refactor project root folder and update package structure

--- demo/src/main/java/com/example/demo/repository/PostitusRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Postitus;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostitusRepository extends JpaRepository<Postitus, Integer> {
-
-}
